572126d0bf1d86c5a5b5a3d1d9e79fa42f8050a2
Jesse Pitel <jesse.pitel@gmail.com>
2016-05-22 16:29:06 -1000

    Split EnemyShipAI into allied and enemy AI. [not complete]

---
+++ Assets/Src/Ship/AllyShipAI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class AllyShipAI : BaseShipAI {
+
+	public override void AssignNewTarget() {
+
+		List<GameObject> enemies = new List<GameObject>(GameObject.FindGameObjectsWithTag("Enemy"));
+		if (Target != null && enemies.Contains(Target))
+			enemies.Remove(Target);
+		// TODO: Create target assignment logic here based on threat.
+		Target = enemies.ElementAt(Mathf.FloorToInt(Random.Range(0, enemies.Count)));
+		TargetRb = Target.GetComponent<Rigidbody2D>();
+	}
+
+}
